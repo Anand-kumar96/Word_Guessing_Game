5d983d12f0839af10847b40b984b2ab3a9677c04
Anand <hyeiamaka06@gmail.com>
2022-12-30 22:34:46 +0530

    guess word

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-// js code
-const inputs = document.querySelector(".inputs"),
-hintTag = document.querySelector(".hint span"),
-guessLeft = document.querySelector(".guess-left span"),
-wrongLetter = document.querySelector(".wrong-letter span"),
-resetBtn = document.querySelector(".reset-btn"),
-typingInput = document.querySelector(".typing-input");
-let word, maxGuesses, incorrectLetters = [], correctLetters = [];
+// all class select
+const inputs = document.querySelector(".inputs");
+const hintTag = document.querySelector(".hint span");
+const guessLeft = document.querySelector(".guess-left span");
+const wrongLetter = document.querySelector(".wrong-letter span");
+const resetBtn = document.querySelector(".reset-btn");
+const typingInput = document.querySelector(".typing-input");
+
+let word, maxGuesses, incorrectLetters = [], correctLetters = []; 
+//function for random word generating
 function randomWord() {
-    let ranItem = wordList[Math.floor(Math.random() * wordList.length)];
-    word = ranItem.word;
-    maxGuesses = word.length >= 5 ? 8 : 6;
-    correctLetters = []; incorrectLetters = [];
-    hintTag.innerText = ranItem.hint;
+    document.body.style.backgroundImage = "none";
+    let randomItem = wordList[Math.floor(Math.random() * wordList.length)];
+    word = randomItem.word;
+    maxGuesses = word.length >= 5 ? 8 : 6; //maximum guess can be given 
+    correctLetters = []; 
+    incorrectLetters = [];
+    hintTag.innerText = randomItem.hint;
     guessLeft.innerText = maxGuesses;
     wrongLetter.innerText = incorrectLetters;
     let html = "";
@@ -21,12 +25,13 @@ function randomWord() {
     }
 }
 randomWord();
+// checking for input
 function initGame(e) {
     let key = e.target.value.toLowerCase();
-    if(key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(` ${key}`) && !correctLetters.includes(key)) {
-        if(word.includes(key)) {
+    if (key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(` ${key}`) && !correctLetters.includes(key)) {
+        if (word.includes(key)) {
             for (let i = 0; i < word.length; i++) {
-                if(word[i] == key) {
+                if (word[i] == key) {
                     correctLetters += key;
                     inputs.querySelectorAll("input")[i].value = key;
                 }
@@ -40,17 +45,34 @@ function initGame(e) {
     }
     typingInput.value = "";
     setTimeout(() => {
-        if(correctLetters.length === word.length) {
-            alert(`Congrats! You found the word ${word.toUpperCase()}`);
-            return randomWord();
-        } else if(maxGuesses < 1) {
-            alert("Game over! You don't have remaining guesses");
-            for(let i = 0; i < word.length; i++) {
+        if (correctLetters.length === word.length) {
+            startConfetti();
+            document.querySelector(".congrats").innerText="Congratulations You Won !"
+        } else if (maxGuesses < 1) {
+            document.querySelector(".congrats").innerText="Game over! No remaining guesses";
+            for (let i = 0; i < word.length; i++) {
                 inputs.querySelectorAll("input")[i].value = word[i];
             }
         }
     }, 100);
 }
+
+//random color generate
+function genetrateRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const randomColors = `rgb(${red},${green},${blue})`
+    return randomColors;
+}
+
+// applying event handler
+resetBtn.addEventListener("click", () => {
+    const randomColor = genetrateRandomColor();
+    document.body.style.backgroundColor = randomColor;
+    document.querySelector(".congrats").innerText="";
+    stopConfetti();
+});
 resetBtn.addEventListener("click", randomWord);
 typingInput.addEventListener("input", initGame);
 inputs.addEventListener("click", () => typingInput.focus());
